5feab6577c3c3c32d412b8c061d401b399565600
fuzzy-logic-projects <kumarnikhil0669work@gmail.com>
2026-08-05 09:02:53 +0530

    Delete portfolio directory

--- portfolio/functions/api/logout.ts
+++
@@ -1,5 +0,0 @@
-import { CLEAR_SESSION_COOKIE } from '../_lib/auth';
-
-export const onRequestPost: PagesFunction = async () => {
-  return Response.json({ ok: true }, { status: 200, headers: { 'Set-Cookie': CLEAR_SESSION_COOKIE } });
-};
